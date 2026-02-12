c61112234dc1a1f2b05fa7f6599d86d9f1aa2448
Bùi Phạm Minh Tuấn <bpmtuan@gmail.com>
2026-02-12 07:52:58 +0700

    buoi16cuoi

--- tests/CRM/pom/CRMDashboardPage.ts
+++ tests/CRM/pom/CRMDashboardPage.ts
@@ -5,12 +5,14 @@ export class CRMDashboardPage extends BasePage {
     private readonly pageLocators = {
         logo: '#logo',
         // Search input
-        searchInput: (page: Page)=> page.getByRole('searchbox', {name: 'Search', exact: true})
+        searchInput: (page: Page)=> page.getByRole('searchbox', {name: 'Search'}),
+        dashboardLink: (page: Page) => page.getByRole('link', {name: 'Dashboard'})
     }
     public element = this.createLocatorGetter(this.pageLocators)
     
     async expectOnPage(): Promise<void> {
-        await expect(this.element('logo')).toBeVisible
-        await expect(this.get(this.pageLocators, 'searchInput')).toBeVisible
+        expect(this.element('logo')).toBeVisible
+        expect(this.get(this.pageLocators, 'searchInput')).toBeVisible
+        expect(this.element("dashboardLink")).toBeVisible
     }
 }
