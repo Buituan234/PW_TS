2063d284b6306a91ab5ced3504f2e60dcdb2424e
Bùi Phạm Minh Tuấn <bpmtuan@gmail.com>
2026-02-23 07:16:15 +0700

    feature/buoi18

---
+++ tests/CRM/pom/CRMNewCustomerPage.ts
@@ -0,0 +1,92 @@
+import { BasePage } from "./BasePage";
+import { Page, expect } from "@playwright/test";
+
+export interface CustomerInfo {
+    company: string,
+    vat?: string,
+    phone?: string,
+    website?: string,
+    address?: string,
+    city?: string,
+    state?: string,
+    zip?: string,
+    country?: string,
+    language?: string,
+    currency?: string,
+}
+
+export class CRMNewCustomerPage extends BasePage {
+    private readonly pageLocators = {
+        //Input fields
+        company: '#company',
+        vat: '#vat',
+        phone: '#phonenumber',
+        website: '#website',
+        address: '#address',
+        city: '#city',
+        state: '#state',
+        zip: '#zip',
+
+        //Button
+
+        saveButton: (page: Page)=> page.locator('//div[@id="profile-save-section"]//button[normalize-space()="Save"]'),
+
+        asterik: (page: Page) => page.locator('label', { hasText: 'Company' }).locator('small', { hasText: '*' }),
+
+        currencyButton: (page: Page) => page.locator('//label[@for="default_currency"]//following-sibling::div[contains(@class,"dropdown")]'),
+
+        countryButton: (page: Page) => page.locator('//label[@for="country"]//following-sibling::div[contains(@class,"dropdown")]'),
+
+        languageButton: (page: Page) => page.locator('//select[@id="default_language"]')
+    } as const
+
+    public element = this.createLocatorGetter(this.pageLocators)
+
+    async fillCompany(name: string) {
+        await this.fillwithlog(this.element('company'), 'name')
+    }
+
+    async contactInfo(info: CustomerInfo){
+        if (info.vat){
+            await this.fillwithlog(this.element('vat'), info.vat)
+        }
+        if (info.phone){
+            await this.fillwithlog(this.element('phone'), info.phone)
+        }
+        if (info.website){
+            await this.fillwithlog(this.element('website'), info.website)
+        }
+    }
+
+    async fillAddress(info: CustomerInfo){
+        if (info.address){
+            await this.fillwithlog(this.element('address'), info.address)
+        }
+        if (info.city){
+            await this.fillwithlog(this.element('city'), info.city)
+        }
+        if (info.state){
+            await this.fillwithlog(this.element('state'), info.state)
+        }
+    }
+
+    async selectCurrency(info: CustomerInfo){
+        if (info.currency){
+            await this.helpers.selectBootstrapOption(this.element('currencyButton'), info.currency)
+        }
+    }
+
+    async selectCountry(info: CustomerInfo){
+        if (info.country){
+            await this.helpers.selectBootstrapOption(this.element('countryButton'), info.country)
+        }
+    }
+
+    async clickSaveButton(){
+        await this.clickwithlog(this.element('saveButton'))
+    }
+
+    async expectOnPage(): Promise<void> {
+        expect(this.element('asterik')).toBeVisible()
+    }
+}
---
+++ tests/CRM/test-crm-new-customer-page.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect, Page } from '@playwright/test'
+import { CRMLoginPage } from './pom/CRMLoginPage'
+import { CRMDashboardPage } from './pom/CRMDashboardPage';
+import { CRMCustomerPage } from './pom/CRMCustomerPage';
+import { CRMNewCustomerPage } from './pom/CRMNewCustomerPage';
+import { createMinimalCustomerInfo } from './utils/test-data'
+
+test.beforeEach(async ({page}) => {
+    const loginPage = new CRMLoginPage(page);
+    await loginPage.goto()
+    await loginPage.expectOnPage()
+    await loginPage.login('admin@example.com','123456');
+})
+
+function createCRMPage(page: Page){
+    return {
+        dashboardPage: new CRMDashboardPage(page),
+        customerPage: new CRMCustomerPage(page),
+        newCustomerPage: new CRMNewCustomerPage(page),
+        //profile
+    }
+}
+
+test('TC_CUST_01- Tạo Customer (Chỉ nhập trường bắt buộc', async({page}) => {
+    const {dashboardPage, customerPage, newCustomerPage} = createCRMPage(page)
+
+    await test.step('Verify dashboard da load sau khi login', async()=>{
+        await dashboardPage.expectOnPage()
+    })
+
+    await test.step('Navigate từ dashboardPage > Customer Page', async()=>{
+        await dashboardPage.navigateMenu('Customers')
+        await customerPage.expectOnPage()
+    })
+
+    await test.step('Navigate từ CustomerPage > new Customer Page', async()=>{
+        await customerPage.clickAddNewCustomer()
+        await newCustomerPage.expectOnPage()
+    })
+    const customerInfo = createMinimalCustomerInfo()
+    await test.step('Fill required compnany field', async()=>{
+        await newCustomerPage.fillCompany(customerInfo.company)
+        await newCustomerPage.clickSaveButton()
+    })
+})
