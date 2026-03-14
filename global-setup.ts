49826df84461e2bafdad2599e8f3dbaef92cb9ee
Bùi Phạm Minh Tuấn <124521898+Buituan234@users.noreply.github.com>
2026-03-14 16:49:20 +0700

    update buoi29

--- global-setup.ts
+++ global-setup.ts
@@ -1,24 +1,23 @@
-import { FullConfig } from '@playwright/test';
-
+import { chromium, FullConfig } from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
+  const browser = await chromium.launch(); // Tự bật browser
 
-  console.log('🌅 [GLOBAL SETUP] Bắt đầu khởi động hệ thống...');
+  const page = await browser.newPage();
 
+  try {
+    await page.goto('https://github.com/login');
 
-  // Giả sử ta khởi động một Server DB ảo mất 1 giây
+    await page.click('input[name="commit-sign-in"]', { timeout: 5000 });
 
-  await new Promise(r => setTimeout(r, 1000));
+    await page.context().storageState({ path: 'auth.json' });
+  } catch (error) {
+    console.error(' Login thất bại!');
 
-
-  // QUAN TRỌNG: Truyền dữ liệu cho các bài Test bằng biến môi trường (Environment Variables)
-
-  process.env.DB_CONNECTION_URL = 'postgres://admin:123456@localhost:5432/my_db';
-
-  process.env.API_PORT = '8080';
-
-  console.log('✅ [GLOBAL SETUP] Đã bật Server tại port 8080. Sẵn sàng!');
-
+    throw error;
+  } finally {
+    await browser.close();
+  }
 }
 
 export default globalSetup;
